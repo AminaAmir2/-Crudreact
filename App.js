5d31f379ecd88bd8627cdff38f9b65ee06980d89
AminaAmir2 <129858786+AminaAmir2@users.noreply.github.com>
2023-04-04 15:06:26 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+
+import StudentForm from './components/StudentForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <StudentForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
